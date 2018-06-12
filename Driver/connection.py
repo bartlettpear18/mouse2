29174d4f2a841935de1bea45b17575f05ef03e74
bartlettpear18 <joelbartlett20@gmail.com>
2018-06-12 16:40:19 -0700

    Began sending accel data

--- Driver/connection.py
+++ Driver/connection.py
@@ -1,6 +1,9 @@
 import bluetooth
 import io
 import sys
+from mouse import Mouse
+
+mbot = Mouse()
 
 service_id ="0b938f75-7fe7-4a04-9079-579d78ad64b7"
 service_classes = [service_id, bluetooth.SERIAL_PORT_CLASS]
@@ -27,6 +30,11 @@ setup()
 while True:
     input = inputSocket.recv(1024)
     data = bytearray(input)
-    for b in data :
-        print b
+    # for b in data :
+    #     print b
+    x = int(data[0])
+    y = int(data[1])
+    mbot.move(x,y)
+    print x, y
 close()
+# mbot.move(10,10)
--- test/app/src/main/java/com/example/joelbartlett/test/MainActivity.java
+++ test/app/src/main/java/com/example/joelbartlett/test/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.joelbartlett.test;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -13,7 +17,7 @@ import java.io.OutputStream;
 import java.util.Set;
 import java.util.UUID;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener{
 
     //Constants
     private String TAG = "debug";
@@ -29,10 +33,40 @@ public class MainActivity extends AppCompatActivity {
     private InputStream inputStream;
     private OutputStream outputStream;
 
+    //Mouse hardware
+    private boolean running = false;
+    private final int SCALAR = 10;
+    private final int DELTA_TIME = 10; //milliseconds
+    private final int X_LIMIT = 1;
+    private final int Y_LIMIT = 1;
+
+    public Integer mouseX = 0;
+    public Integer mouseY = 0;
+
+
+    private int prevPosX = 0;
+    private int prevPosY = 0;
+
+    private int currentPosX;
+    private int currentPosY;
+
+    private int xVel0 = 0;
+    private int yVel0 = 0;
+
+    //Sensor setup
+    private SensorManager sensors;
+    private final double Z_LIMIT = 0.1;
+    private float xAccel;
+    private float yAccel;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sensors = (SensorManager) getSystemService(SENSOR_SERVICE);
+        sensors.registerListener(this, sensors.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION), SensorManager.SENSOR_DELAY_NORMAL);
+        running = true;
 
         adapter = BluetoothAdapter.getDefaultAdapter();
         adapter.enable();
@@ -46,18 +80,21 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
 
+
     }
 
     @Override
     public void onStop() {
         super.onStop();
         try {
+            running = false;
             terminate();
             Log.d(TAG, "Connection terminated");
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
     /**
      * Find BluetoothDevice of Server
      * @param serverMAC
@@ -85,13 +122,7 @@ public class MainActivity extends AppCompatActivity {
         outputStream = socket.getOutputStream();
         Log.d(TAG, "Streams connected");
 
-        Integer i = 5;
-        Integer j = 6;
-        Integer k = 7;
-        byte[] packet = {i.byteValue(),j.byteValue(), k.byteValue()};
-        outputStream.write(packet);
-        Log.d(TAG, "Packet sent: " + packet.toString());
-
+        sendPackets.start();
     }
 
     /**
@@ -104,4 +135,59 @@ public class MainActivity extends AppCompatActivity {
         outputStream.close();
     }
 
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if(event.sensor.getType() == Sensor.TYPE_LINEAR_ACCELERATION) {
+            if(event.values[2] < Z_LIMIT) {
+                xAccel = event.values[0];
+                yAccel = event.values[1];
+//                Log.d(TAG, xAccel + ", " + yAccel);
+
+            } else {
+                xAccel = 0;
+                yAccel = 0;
+//                Log.d(TAG, "0, 0");
+            }
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
+    Thread sendPackets = new Thread(new Runnable() {
+        @Override
+        public void run() {
+            while(true) {
+
+
+                if(xAccel > X_LIMIT) {
+                    currentPosX = (int) (xAccel * DELTA_TIME * DELTA_TIME)/2 + (xVel0 * DELTA_TIME);
+                    xVel0 = (int) (xVel0 + xAccel * DELTA_TIME);
+                    mouseX = (currentPosX - prevPosX)/10;
+                    prevPosX = currentPosX;
+                } else {
+                    mouseX = 0;
+                }
+
+                if(yAccel > Y_LIMIT) {
+                    currentPosY = (int) (yAccel * DELTA_TIME * DELTA_TIME)/2 + (yVel0 * DELTA_TIME);
+                    yVel0 = (int) (yVel0 + yAccel * DELTA_TIME);
+                    mouseY = (currentPosY - prevPosY)/10;
+                    prevPosY = currentPosY;
+                } else {
+                    mouseY = 0;
+                }
+
+                byte[] packet = {mouseX.byteValue(), mouseY.byteValue()};
+                try {
+                    outputStream.write(packet);
+                    Log.d(TAG, "Sent packet: " + packet[0] + ", " + packet[1]);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    });
 }
